f484faab832b539f082a878db1014279a1fbe1e6
itzikbs1 <92460446+itzikbs1@users.noreply.github.com>
2022-02-26 12:56:19 +0200

    Add files via upload

--- Client.py
+++ Client.py
@@ -4,8 +4,11 @@ import tkinter
 import tkinter.scrolledtext
 from tkinter import simpledialog
 from tkinter import *
+import os
+import shutil
 
-HOST = '127.0.0.1'
+# HOST = '127.0.0.1'
+HOST = socket.gethostbyname(socket.gethostname())
 PORT = 5001
 
 
@@ -13,17 +16,13 @@ class Client:
     def __init__(self, host, port):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((host, port))
-        msg = tkinter.Tk()
-        msg.withdraw()
+        self.file_name_client = ""
         self.name = ""
-        # self.name = simpledialog.askstring("Name", "Set a name", parent=msg)
-        # self.name = simpledialog.askstring("Name", "Set a name")
+        self.data_f = ""
         self.gui = False
         self.running = True
         gui_thread = threading.Thread(target=self.gui_loop)
-        # recv_thread = threading.Thread(target=self.receive)
         gui_thread.start()
-        # recv_thread.start()
 
     def gui_loop(self):
         self.window = tkinter.Tk()
@@ -36,13 +35,6 @@ class Client:
         self.text = tkinter.scrolledtext.ScrolledText(self.window)
         self.text.pack(padx=20, pady=5)
         self.text.config(state='disabled')
-
-        # self.msg = tkinter.Label(self.window, text="Message:", bg="lightgray")
-        # self.msg.config(font=("Ariel", 12))
-        # self.msg.pack(padx=20, pady=5)
-
-        # self.area = tkinter.Text(self.window, height=3)
-        # self.area.pack(padx=20, pady=5)
 
         self.send_button = tkinter.Button(self.window, text="Send", command=self.write)
         self.send_button.config(font=("Ariel", 12))
@@ -57,9 +49,19 @@ class Client:
         self.dis_button.pack(padx=20, pady=5)
 
 
-        self.login_button = tkinter.Button(self.window, text="Connect", command=self.login)
-        self.login_button.config(font=("Ariel", 12))
-        self.login_button.pack(padx=20, pady=5)
+        self.connect_button = tkinter.Button(self.window, text="Connect", command=self.connect)
+        self.connect_button.config(font=("Ariel", 12))
+        self.connect_button.pack(padx=20, pady=5)
+
+
+        self.get_files_names = tkinter.Button(self.window, text="Get Files", command=self.get_files)
+        self.get_files_names.config(font=("Ariel", 12))
+        self.get_files_names.pack(padx=20, pady=5)
+
+        self.download_files = tkinter.Button(self.window, text="Download File", command=self.get_name_file)
+        self.download_files.config(font=("Ariel", 12))
+        self.download_files.pack(padx=20, pady=5)
+
 
         self.to_label = tkinter.Label(self.window, text="To(blank to all):", bg="lightgray")
         self.to_label.config(font=("Ariel", 12))
@@ -78,6 +80,26 @@ class Client:
         self.window.protocol("VM_DELETE_WINDOW", self.stop)
         self.window.mainloop()
 
+
+    def get_files(self):
+        self.sock.send('GET_FILES'.encode('utf-8'))
+
+    def get_name_file(self):
+        msg = tkinter.Tk()
+        msg.withdraw()
+        file_name_server = simpledialog.askstring("Server File Name", "Set a Server File", parent=msg)
+        self.file_name_client = simpledialog.askstring("Client File Name", "Set a Client File", parent=msg)
+        self.sock.send(file_name_server.encode('utf-8') + "#".encode('utf-8'))# + self.file_name_client.encode('utf-8'))
+        # self.create_file(file_name_client)
+
+    def create_file(self, file_name_client):
+        file = open(file_name_client, 'wb')
+        if self.data_f != "":
+            file.write(self.data_f.encode('utf-8'))
+        file.close()
+        print("File Has Been Succesfully!")
+
+
     def stop(self):
         self.running = False
         self.window.destroy()
@@ -88,9 +110,12 @@ class Client:
         while self.running:
             try:
                 msg = self.sock.recv(1024).decode('utf-8')
-                print(msg)
                 if msg == 'NICK':
                     self.sock.send(self.name.encode('utf-8'))##'ASCI'
+
+                elif msg.startswith("data:"):
+                    self.data_f = msg[5:]
+                    self.create_file(self.file_name_client)
                 else:
                     if self.gui:
                         self.text.config(state='normal')
@@ -101,12 +126,10 @@ class Client:
                 break
             except:
                 print("ERROR")
-                # print("88")
                 self.sock.close()
-                # break
                 exit(0)
 
-    def login(self):
+    def connect(self):
         msg = tkinter.Tk()
         msg.withdraw()
         self.name = simpledialog.askstring("Name", "Set a name", parent=msg)
@@ -118,60 +141,16 @@ class Client:
 
     def disconnect(self):
         self.sock.send('DIS'.encode('utf-8'))
-        self.window.destroy()
+        # self.window.destroy()
+        self.window.quit()
 
     def write(self):
         while True:
             msg = '{}: {}'.format(self.name, self.msg_area.get('1.0', 'end'))
             to = '{}'.format(self.to_area.get('1.0', 'end'))
             self.sock.send(to.encode('utf-8') + "|".encode('utf-8') + msg.encode('utf-8'))
-            # self.sock.send(to.encode('utf-8'))
             self.msg_area.delete('1.0', 'end')
             self.to_area.delete('1.0', 'end')
             break
-            # print("125")
-            # print("msg: ",msg)
-            # print()
-            # print("to: ", to)
-            # print("len to: ", len(to))
-            # if msg[len(self.name) + 2:].startswith('/'):
-            #     print("81")
-            #     if msg[len(self.name) + 2:].startswith('/get_users'):
-            #         print("83")
-            #         self.sock.send('GET_USERS'.encode('utf-8'))
-            #         break
-            #     elif msg[len(self.name) + 2:].startswith('/disconnect'):
-            #         print("86")
-            #         self.sock.send('DIS'.encode('utf-8'))
-            #         break
-            #     elif msg[len(self.name) + 2:].startswith('/connect'):
-            #         print("89")
-            #         self.sock.send('{} CON'.format(msg[len(self.name) + 2 + 10:]).encode('utf-8'))
-            #         break
-            #     elif msg[len(self.name) + 2:].startswith('/set_msg_all'):
-            #         print("92")
-            #         self.sock.send('{} SEND_ALL'.format(msg[len(self.name) + 2 + 14:]).encode('utf-8'))
-            #         break
-            #     elif msg[len(self.name) + 2:].startswith('/set_msg'):
-            #         print("95")
-            #         self.sock.send('{} SEND_ONE'.format(msg[len(self.name) + 2 + 10:]).encode('utf-8'))
-            #         break
-            # else:
-                # if len(to) == 1:
-                #     print("149")
-                #     self.sock.send(msg.encode('utf-8'))
-                #     # self.sock.send(to.encode('utf-8'))
-                #     # print("hey!!!!!!!!!")
-                #     self.msg_area.delete('1.0', 'end')
-                #     self.to_area.delete('1.0', 'end')
-                #     # self.sock.se
-                #     break
-
-                    # self.sock.sendto(msg.encode('utf-8'), name)
 
 client = Client(HOST, PORT)
-# thread1 = threading.Thread(target=receive)
-# thread1.start()
-#
-# thread2 = threading.Thread(target=write)
-# thread2.start()
--- Server.py
+++ Server.py
@@ -1,10 +1,12 @@
 import threading
 import socket
 from AsyncMessages import AsyncMessages
-HOST = '127.0.0.1'
+import os
+# HOST = '127.0.0.1'
+HOST = socket.gethostbyname(socket.gethostname())
 PORT = 5001
 
-class Server(Asyn):
+class Server():
 
     def __init__(self, host, port):
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -14,38 +16,52 @@ class Server(Asyn):
         self.names = [] #{name, socket}
         # self.conn = False
         # self.async_msgs = {}
-        self.list_msgs = AsyncMessages()
+        # self.list_msgs = {}
 
 
     def broadcast(self, msg):
         for client in self.clients:
             client.send(msg)
-        self.list_msgs.put_msg_to_all(msg)
+        # self.list_msgs.put_msg_to_all(msg)
 
     def pr_broadcast(self,from_client, to_client, msg):
         for c in self.clients:
             name = self.client_name(c)
-
             if name == to_client:
                 c.send(msg.encode('utf-8'))
                 from_client.send(msg.encode('utf-8'))
-        self.list_msgs.put_msg_by_user(msg, to_client)
+        # self.list_msgs.put_msg_by_user(msg, to_client)
 
     def handle(self, client):
         while True:
             try:
                 start = client.recv(1024)
-                temp = start.decode('utf-8').partition("|")
-
-                if len(temp[1]) != 0:
+                temp = start.decode('utf-8')#.partition("|")
+                c1 = "|"
+                c2 = "#"
+                server_file_name = ""
+                # client_file_name = ""
+                to = ""
+                msg = ""
+                if c1 in temp:
+                    temp = temp.partition("|")
                     to = temp[0]
                     msg = temp[2]
+                elif c2 in temp:
+                    temp = temp.partition("#")
+                    server_file_name = temp[0]
+                    # client_file_name = temp[2]
                 else:
+                    temp = temp.partition("|")
                     msg = temp[0]
                     to = temp[2]
 
+
                 if msg.startswith('GET_USERS'):
                     self.print_users(client)
+
+                elif msg.startswith('GET_FILES'):
+                    self.get_files(client)
 
                 elif msg.startswith('DIS'):
                     self.dis_user(client)
@@ -54,6 +70,9 @@ class Server(Asyn):
                 else:
                     if len(to) == 1:
                         self.broadcast(msg.encode('utf-8'))
+
+                    elif len(server_file_name) != 0:
+                        self.download_file(client, server_file_name)
 
                     else:
                         self.pr_broadcast(client, to[:-1], msg)#.encode('utf-8'))
@@ -66,45 +85,25 @@ class Server(Asyn):
                     name = self.names[index]
                     self.broadcast('{} left!'.format(name).encode('utf-8'))
                     self.names.remove(name)
-                    self.list_msgs.delete_socket(client)
-                    self.list_msgs.delete_user()
-                    break
-
-    # def handle(client):
-    #     while True:
-    #         try:
-    #             msg = client.recv(1024)
-    #             print(f"{names[clients.index(client)]} says {msg}")
-    #             # print("{} says {}".format(names[clients.index(client)], msg))
-    #             broadcast(msg)
-    #         except:
-    #             index = clients.index(client)
-    #             clients.remove(client)
-    #             client.close()
-    #             name = names[index]
-    #             names.remove(name)
-    #             break
+                    # self.list_msgs.delete_socket(client)
+                    # self.list_msgs.delete_user()
 
 
     def receive(self):
         while True:
-            # if conn:
             client, addr = self.server.accept()
-            print("Connected with {}".format(str(addr)))
+
 
             client.send('NICK'.encode('utf-8'))
             name = client.recv(1024).decode('utf-8')
+            print("{} Connected with {}".format(name, str(addr)))
             self.names.append(name)
             self.clients.append(client)
-            self.list_msgs.add_new_socket(client)
-            self.list_msgs.add_new_socket_by_user(name)
-            print("Nickname is {}".format(name))
             self.broadcast("{} joined!\n".format(name).encode('utf-8'))
             client.send('Connected to the chat\n'.encode('utf-8'))
 
             thread = threading.Thread(target=self.handle, args=(client,))
             thread.start()
-            # conn = False
 
 
     def client_name(self, client):
@@ -114,22 +113,39 @@ class Server(Asyn):
             return name
 
 
-    def connect_user(name):
-        pass
-
-
     def dis_user(self, client):
         name = self.client_name(client)
         if name in self.names:
+            print("{} Disconnected".format(str(name)))
             client.close()
-            clients.remove(client)
+            self.clients.remove(client)
             self.names.remove(name)
-            self.list_msgs.delete_socket(client)
-            self.list_msgs.delete_user(name)
+            # self.list_msgs.delete_socket(client)
+            # self.list_msgs.delete_user(name)
 
     def print_users(self, client):
         for name in self.names:
+            print("{}".format(name))
             client.send("{}\n".format(name).encode('utf-8'))
+        print("num of users: {}".format(len(self.names)))
+
+
+    def get_files(self,client):
+        dir_path = os.getcwd()
+        dir_list = os.listdir(dir_path)
+        for name in dir_list:
+            if name.endswith(".txt"):
+                client.send("{}\n".format(name).encode('utf-8'))
+
+    def download_file(self, client, server_file_name):
+        file = open(server_file_name, 'rb')
+        file_data = file.read(1024)
+        while file_data:
+            # print("file data!!: " ,file_data)
+            client.send("data:".encode('utf-8') + file_data)
+            file_data = file.read(1024)
+        print("Data is sent!")
+
 
 
 server = Server(HOST,PORT)
